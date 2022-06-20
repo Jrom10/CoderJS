9a7b1374fcc2874f3ef7117983fb92943168425f
Nicolas Lerner <lerner.nicolas@gmail.com>
2022-06-20 12:32:25 -0300

    primer commit

---
+++ Javascript.js
@@ -0,0 +1,20 @@
+// let nombre = prompt ("ingrese su nombre")
+// let correcto = nombre + " " + "ingresado"
+// alert(correcto)
+
+// if (nombre=="tincho"){
+//     alert("sos PETE")
+// }else {
+//     alert("estas Sobrado")
+// }
+
+// let numero = 6
+// let nan = (numero > 5)
+// if(nan==true){
+//     alert ("es true mi rey")
+// }
+
+let entrada = prompt ("ingrese dato")
+while(entrada!="ESC"){
+    alert("el usuario ingreso" + entrada)
+}
