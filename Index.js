cab3598b04960c78577525d270193c8a126b0afb
Nicolas Lerner <lerner.nicolas@gmail.com>
2022-07-18 22:33:49 -0300

    commit 18/7

--- Index.js
+++ Index.js
@@ -1,50 +1,82 @@
 
-const corcel = {nombre: "Corcel Celestial", valor: 150, tipo: "Montura"}
-const dragon = {nombre: "Dragon Alanegra", valor: 250, tipo: "Montura"}
-const atracacielos = {nombre: "Atracacielos", valor: 350, tipo: "Montura"}
+const corcel = {nombre: "Corcel Celestial", valor: 150, tipo: "Montura", id:"4"}
+const dragon = {nombre: "Dragon Alanegra", valor: 250, tipo: "Montura", id:"5"}
+const atracacielos = {nombre: "Atracacielos", valor: 350, tipo: "Montura", id:"6"}
 
 const monturas = [corcel,dragon,atracacielos]
 
-const felino = {nombre: "Crepusculin", valor: 50, tipo: "Mascota"}
-const can = {nombre: "Can de Trasiego", valor: 45, tipo: "Mascota"}
-const tortu = {nombre: "Tortusiu", valor: 60, tipo: "Mascota"}
+const felino = {nombre: "Crepusculin", valor: 50, tipo: "Mascota", id:"1"}
+const can = {nombre: "Can de Trasiego", valor: 45, tipo: "Mascota", id:"2"}
+const tortu = {nombre: "Tortusiu", valor: 60, tipo: "Mascota", id:"3"}
+
+const faccion = {nombre: "Cambio de Faccion", valor:500, tipo: "servicio", id:"7"}
 
 const mascotas = [felino,can,tortu]
+const servicio = [faccion]
 
-const productos = mascotas.concat(monturas)
-
-const nuevaMontura = []
-const nuevaMascota = []
-const nuevoServicio =[]
+let productosSuma = mascotas.concat(monturas, servicio)
+console.log(productosSuma)
 
 let formulario
 let inputNombre
 let inputValor
 let inputTipo
-let tablaMonturas
-let tablaMascotas
-let tablaServicio
+let tablaCompra
+let oferta
+let botonAdmin
+let botonAgregar
+let limpiar
+let terminarCompra
+let total
+
+function generarId() {
+    let id = 8
+    for (let i = 8; i <= productosSuma.length+1; i++){
+    id = i}
+    return id
+}
 
 class Producto {
-    constructor(nombre, valor, tipo) {
+    constructor(nombre, valor, tipo, id) {
         this.nombre = nombre
         this.valor = valor
         this.tipo = tipo
+        this.id = generarId(id)
     }
 }
+
+class Carrito {
+    constructor(id) {
+        this.id = id
+        this.productos = []
+    }
+
+    calcularTotal() {
+        let total = 0
+        for(let i = 0; i < this.productos.length; i++) {
+            total = total + this.productos[i].valor
+        }
+        return total
+    }
+}
+
 
 function inicializarElementos() {
 formulario = document.getElementById("formulario")
 inputNombre = document.getElementById("inputNombre")
 inputValor = document.getElementById("inputValor")
 inputTipo = document.getElementById("inputTipo")
-tablaMonturas = document.getElementById("tablaMonturas")
-tablaMascotas = document.getElementById("tablaMascotas")
-tablaServicio = document.getElementById("tablaServicio")
+tablaCompra = document.getElementById("tablaCompra")
+oferta = document.getElementById("oferta")
+botonAgregar = document.getElementsByClassName("botonAgregar")
+botonAdmin = document.getElementById("botonAdmin")
+limpiar = document.getElementById("limpiar")
+terminarCompra = document.getElementById("terminarCompra")
+total = document.getElementById ("total")
 }
 
 function inicializarEventos(){
-formulario.onsubmit = (event) => validarFormulario(event);
+    formulario.onsubmit = (event) => validarFormulario(event)
 }
 
 function validarFormulario(event) {
@@ -53,280 +85,135 @@ function validarFormulario(event) {
     let valor = parseFloat(inputValor.value)
     let tipo = inputTipo.value
     let nuevoProducto = new Producto (nombre, valor, tipo)
+    
+    if ((tipo==="Montura") && (valor !== "") && (nombre !== "")){
+        monturas.push(nuevoProducto)
+        productosSuma = mascotas.concat(monturas, servicio)
+        mostrarCards()
+    }
+    else if ((tipo==="Mascota") && (valor !== "") && (nombre !== "")) {
+        mascotas.push(nuevoProducto)
+        productosSuma = mascotas.concat(monturas, servicio)
+        mostrarCards()
+    }
+    else if ((tipo==="Servicio") && (valor !== "") && (nombre !== "")) {
+        servicio.push(nuevoProducto)
+        productosSuma = mascotas.concat(monturas, servicio)
+        mostrarCards()
+    }
+    formulario.reset()
+    let st = actualizarCarrito()
+    agregarCarrito(st)
+}
 
-    if (tipo==="Montura"){
-        nuevaMontura.push(nuevoProducto)
-        formulario.reset()
-        limpiarTablaMonturas()
-        agregarProductosMontura()
-    }
-    else if (tipo==="Mascota") {
-        nuevaMascota.push(nuevoProducto)
-        formulario.reset()
-        limpiarTablaMascotas()
-        agregarProductosMascota()
-    }
-    else{
-        nuevoServicio.push(nuevoProducto)
-        formulario.reset()
-        limpiarTablaServicio()
-        agregarProductosServicio()
-    }
+function renderCard(producto) {
+    let cardRendered = `    
+    <div class="card m-3" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">${producto.id}-${producto.nombre}</h5>
+            <p class="card-text">$ ${producto.valor}</p>
+            <a href="#" class="btn btn-primary botonAgregar" id=${producto.id}>Agregar al carrito</a>
+        </div>
+    </div>
+    `
+    return cardRendered
+}
+
+function renovarStorage (productos) {
+    localStorage.clear()
+    localStorage.setItem("productos", JSON.stringify(productos))
 }
 
 
-function agregarProductosMontura() {
-    for (const item of nuevaMontura) {
-        let itemListaMontura = document.createElement("tr")
-        itemListaMontura.innerHTML = `
-            <td>${item.nombre}</td>
-            <td>$${item.valor}</td>
-            <td>${item.tipo}</td>`
-        tablaMonturas.tBodies[0].append(itemListaMontura)
-    }
+function mostrarCards() {
+    oferta.innerHTML = ""
+    productosSuma.forEach(productos => {
+        oferta.innerHTML += renderCard(productos)
+    })
 }
 
-function agregarProductosMascota() {
-    for (const item of nuevaMascota) {
-        let itemListaMascota = document.createElement("tr")
-        itemListaMascota.innerHTML = `
-            <td>${item.nombre}</td>
-            <td>$${item.valor}</td>
-            <td>${item.tipo}</td>`
-        tablaMascotas.tBodies[0].append(itemListaMascota)
-        }
-}
-
-function agregarProductosServicio() {
-    for (const item of nuevoServicio) {
-        let itemListaServicio = document.createElement("tr")
-        itemListaServicio.innerHTML = `
-            <td>${item.nombre}</td>
-            <td>$${item.valor}</td>
-            <td>${item.tipo}</td>`
-        tablaServicio.tBodies[0].append(itemListaServicio)
-        }
-}
-
-function limpiarTablaMonturas() {
-    while (tablaMonturas.rows.length > 1) {
-        tablaMonturas.deleteRow(1)
-    }
-}
-
-function limpiarTablaMascotas() {
-    while (tablaMascotas.rows.length > 1) {
-        tablaMascotas.deleteRow(1)
-    }
-}
-
-function limpiarTablaServicio() {
-    while (tablaServicio.rows.length > 1) {
-        tablaServicio.deleteRow(1)
-    }
-}
 
 function main() {
     inicializarElementos()
     inicializarEventos()
-    agregarProductos()
 }
 
-main();
+main()
 
 
-
-let listaMascotas = document.getElementById(masco)
-for (const item of mascotas) {
-    let itemLista = document.createElement("li")
-    itemLista.innerHTML = `<b>Producto:</b> ${item.nombre}  -  <b>Valor:</b> $${item.valor}  -  <b>Tipo:</b> ${item.tipo}`
-    masco.appendChild(itemLista)
-}
-
-let listaMonturas = document.getElementById(montu)
-for (const item of monturas) {
-    let itemLista = document.createElement("li")
-    itemLista.innerHTML = `<b>Producto:</b> ${item.nombre}  -  <b>Valor:</b> $${item.valor}  -  <b>Tipo:</b> ${item.tipo}`
-    montu.appendChild(itemLista)
-}
-
-for (const item of mascotas) {
-    let itemLista = document.createElement("li")
-    itemLista.innerHTML = `<b>Producto:</b> ${item.nombre}  -  <b>Valor:</b> $${item.valor}  -  <b>Tipo:</b> ${item.tipo}`
-    masco.appendChild(itemLista)
+function agregarCarrito (st) {
+    let carrito = new Carrito(1)
+    if(st !== null){
+        for (const item of st.productos) {
+            carrito.productos.push(item)
+            limpiarCarrito()
+            renderizarCarrito(carrito)
+            renovarCarrito(carrito)
+        }
     }
 
-function nuevoProducto() {
-    let numeroProductos = parseInt(prompt("Cantidad de productos a registrar"))
-    let ingresados = []
-    for (let index = 0; index < numeroProductos ; index++){
-        let nombre = prompt ("Ingrese nombre del producto")
-        let valor = prompt ("ingrese Valor del producto")
-        let productoARegistrar = new Producto(
-            nombre,
-            valor,
-        )
-        ingresados.push(productoARegistrar)
+    let arrayDeBotones = Array.from(botonAgregar)
+    arrayDeBotones.forEach(boton => {
+        boton.addEventListener("click", (e) => {
+            let productoSeleccionado = productosSuma.find(producto => producto.id == e.target.id)
+            carrito.productos.push(productoSeleccionado)
+            limpiarCarrito()
+            renderizarCarrito(carrito)
+            renovarCarrito(carrito)
+        })
+    })
+
+    limpiar.onclick = () => {eliminarCarrito()}
+    terminarCompra.onclick = () => {finalizarCompra(carrito)}
+}
+
+function actualizarCarrito (){
+    let storage = JSON.parse(localStorage.getItem("carrito"))
+    localStorage.removeItem("carrito")
+    return storage
+}
+
+function renovarCarrito(carrito) {
+    localStorage.removeItem("carrito")
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+}
+
+function limpiarCarrito() {
+    tablaCompra.innerHTML = ""
+}
+
+function eliminarCarrito() {
+    localStorage.removeItem("carrito")
+    tablaCompra.innerHTML = ""
+    let st = actualizarCarrito()
+    agregarCarrito(st)
+}
+
+function renderizarCarrito(carrito) {
+    carrito.productos.forEach(producto => {
+        let filaTabla = document.createElement("tr")
+        filaTabla.innerHTML = `
+            <td class="text-center">${producto.nombre}</td>
+            <td class="text-center">$${producto.valor}</td>
+            <td class="text-center">${producto.tipo}</td>`
+            tablaCompra.appendChild(filaTabla)
+    })
+
+    let filaTotal = document.createElement("tr")
+    filaTotal.innerHTML = `
+        <td class="text-center"><b>TOTAL</b></td>
+        <td class=""></td>
+        <td class="text-center m-5"><b>$${carrito.calcularTotal()}</b></td>`
+    tablaCompra.appendChild(filaTotal)
+}
+
+function finalizarCompra(carrito){
+    if (carrito.calcularTotal() !==0 ){
+        total.innerHTML = `
+            Usted debera abonar: <b>$${carrito.calcularTotal()}</b>. Sera redirigido para realizar el pago.`
     }
-    return ingresados
-}
-
-
-let nombre = prompt ("Bienvenido a nuestra tienda. Ingrese su nombre.'Esc' para terminar")
-
-if (nombre === "ADMIN"){
-    let pass = prompt ("Ingrese contraseña")
-        if (pass === "ADMIN"){
-            let returnAdmin = menuAdmin()
-            nombre = prompt ("Ingrese nombre de usuario")
-            menuUser(returnAdmin)
-        }
-        else {
-            alert("Contraseña incorrecta")
-        }
-}
-else if (nombre !== "" && nombre !== "esc" && nombre !== "ESC") {
-    menuUser()
-}
-else{
-    alert ("Ingrese una opcion valida")
-    ESC
-}
-
-
-function menuAdmin() {
-    alert("Menu para ingresar nuevos productos")
-    const agregado = productos.concat(nuevoProducto())
-    console.log(agregado)
-    return agregado
-}
-
-function menuUser(){
-    let opcion = 0;
-    
-    do{
-        opcion = parseInt(prompt(`Bienvenido ${nombre}\nSeleccione una opcion para continuar. (ESC para salir)\n1-Comprar\n2-Pagar\n3-Eliminar compras`))
-    
-    switch (opcion) {
-        case 1:
-            agregarProducto()
-            menuUser()
-        case 2:
-            metodoPago()
-            mostrarTotal(totalProductos)
-        case 3:
-            reinicioCarrito()
-            menuUser()
-        default: 
-            alert("seleccione una opcion valida")
-            ESC
-        } 
-    } while (opcion !== "")
-}
-
-function agregarProducto() {
-    let seleccionarProducto = 0
-    do {
-        seleccionarProducto = parseInt(prompt(`Seleccione el producto que desea agregar al carrito.\n su total hasta ahora es $${totalProductos}\n1-Monturas\n2-Mascotas\n3-Cambio de Faccion\n0-Finallizar Compra`))
-
-        if (seleccionarProducto === 1) {
-            agregarMontura()
-        }
-        if (seleccionarProducto === 2) {
-            agregarMascota()
-        }
-        if (seleccionarProducto === 3) {
-            totalProductos = suma(totalProductos, valorFaccion)
-            carrito.push
-            alert ("producto añadido")
-        }
-        if (seleccionarProducto >= 4) {
-            alert("Seleccione una opcion valida")
-        }
-
-    }while (seleccionarProducto !== 0)
-
-}
-
-function agregarMontura() {
-    let seleccionarMontura = 0
-    do {
-        seleccionarMontura = parseInt(prompt(`Seleccione montura.\n1-Corcel Celestial\n2-dragon Alanegra\n3-Atracacielos\n0-Finallizar Compra / Volver al menu anterior`))
-
-        if (seleccionarMontura === 1) {
-            totalProductos = suma(totalProductos, corcel.valor)
-            carrito.push
-            alert ("producto añadido")
-        }if (seleccionarMontura === 2) {
-            totalProductos = suma(totalProductos, dragon.valor)
-            carrito.push
-            alert ("producto añadido")
-        }if (seleccionarMontura === 3) {
-            totalProductos = suma(totalProductos, atracacielos.valor)
-            carrito.push
-            alert ("producto añadido")
-        }if (seleccionarMontura >= 4) {
-            alert("Seleccione una opcion valida")
-        }
-
-    }while (seleccionarMontura !== 0)
-
-}
-
-function agregarMascota() {
-    let seleccionarMascota = 0
-    do {
-        seleccionarMascota = parseInt(prompt(`Seleccione montura.\n1-Crepusculin\n2-Can de Trasiego\n3-Tortusiu\n0-Finallizar Compra / Volver al menu anterior`))
-
-        if (seleccionarMascota === 1) {
-            totalProductos = suma(totalProductos, felino.valor)
-            carrito.push
-            alert ("producto añadido")
-        }if (seleccionarMascota === 2) {
-            totalProductos = suma(totalProductos, can.valor)
-            carrito.push
-            alert ("producto añadido")
-        }if (seleccionarMascota === 3) {
-            totalProductos = suma(totalProductos, tortu.valor)
-            carrito.push
-            alert ("producto añadido")
-        }if (seleccionarMascota >= 4) {
-            alert("Seleccione opcion valida")
-        }
-
-    }while (seleccionarMascota !== 0)
-
-}
-
-function mostrarTotal(resultado) {
-    alert(`El total a pagar es: $${resultado}\n Gracias por su compra`)
-}
-
-function metodoPago() {
-    let transferencia = totalProductos
-    let efectivo = totalProductos * 0.8
-    let credito = totalProductos * 1.2
-    let metodoPago = 0
-
-    metodoPago = parseInt(prompt(`El total en el carrito es de: $${totalProductos} \nComo desea pagarlo?\n 1.Efectivo\n 2.Transferencia\n 3.Tarjeta de credito`))
-        if (metodoPago === 1) {
-            alert("Se ha aplicado un descuento del 20%")
-            totalProductos = efectivo
-
-        }if (metodoPago === 2) {
-            totalProductos = transferencia
-
-        }if (metodoPago === 3) {
-            alert("Se ha aplicado un recargo del 20%")
-            totalProductos = credito
-
-        }if (metodoPago >= 4) {
-            alert("Seleccione una opcion valida")            
-        }
-}
-
-function reinicioCarrito() {
-    alert("Se eliminaron los productos")
-    totalProductos = multiplicacion(totalProductos, 0)
-    carrito = []
+    else {
+        total.innerHTML = `
+            <b>Su carrito esta vacio.<b>`
+    }
 }
