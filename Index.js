858fdf87a24a50eb4562f4da2422b687ecb8fe7b
Nicolas Lerner <lerner.nicolas@gmail.com>
2022-07-25 15:19:09 -0300

    commit 25/7

--- Index.js
+++ Index.js
@@ -28,13 +28,8 @@ let botonAgregar
 let limpiar
 let terminarCompra
 let total
+let alertCarrito
 
-function generarId() {
-    let id = 8
-    for (let i = 8; i <= productosSuma.length+1; i++){
-    id = i}
-    return id
-}
 
 class Producto {
     constructor(nombre, valor, tipo, id) {
@@ -43,6 +38,13 @@ class Producto {
         this.tipo = tipo
         this.id = generarId(id)
     }
+}
+
+function generarId() {
+    let id = 8
+    for (let i = 8; i <= productosSuma.length+1; i++){
+    id = i}
+    return id
 }
 
 class Carrito {
@@ -73,6 +75,7 @@ botonAdmin = document.getElementById("botonAdmin")
 limpiar = document.getElementById("limpiar")
 terminarCompra = document.getElementById("terminarCompra")
 total = document.getElementById ("total")
+alertCarrito = document.getElementById ("")
 }
 
 function inicializarEventos(){
@@ -89,16 +92,19 @@ function validarFormulario(event) {
     if ((tipo==="Montura") && (valor !== "") && (nombre !== "")){
         monturas.push(nuevoProducto)
         productosSuma = mascotas.concat(monturas, servicio)
+        alertRegistro()
         mostrarCards()
     }
     else if ((tipo==="Mascota") && (valor !== "") && (nombre !== "")) {
         mascotas.push(nuevoProducto)
         productosSuma = mascotas.concat(monturas, servicio)
+        alertRegistro()
         mostrarCards()
     }
     else if ((tipo==="Servicio") && (valor !== "") && (nombre !== "")) {
         servicio.push(nuevoProducto)
         productosSuma = mascotas.concat(monturas, servicio)
+        alertRegistro()
         mostrarCards()
     }
     formulario.reset()
@@ -133,14 +139,6 @@ function mostrarCards() {
 }
 
 
-function main() {
-    inicializarElementos()
-    inicializarEventos()
-}
-
-main()
-
-
 function agregarCarrito (st) {
     let carrito = new Carrito(1)
     if(st !== null){
@@ -155,6 +153,7 @@ function agregarCarrito (st) {
     let arrayDeBotones = Array.from(botonAgregar)
     arrayDeBotones.forEach(boton => {
         boton.addEventListener("click", (e) => {
+            alertAgregar()
             let productoSeleccionado = productosSuma.find(producto => producto.id == e.target.id)
             carrito.productos.push(productoSeleccionado)
             limpiarCarrito()
@@ -163,7 +162,7 @@ function agregarCarrito (st) {
         })
     })
 
-    limpiar.onclick = () => {eliminarCarrito()}
+    limpiar.onclick = () => {LimpiarCompra(carrito)}
     terminarCompra.onclick = () => {finalizarCompra(carrito)}
 }
 
@@ -183,6 +182,7 @@ function limpiarCarrito() {
 }
 
 function eliminarCarrito() {
+    alertLimpiar()
     localStorage.removeItem("carrito")
     tablaCompra.innerHTML = ""
     let st = actualizarCarrito()
@@ -208,12 +208,103 @@ function renderizarCarrito(carrito) {
 }
 
 function finalizarCompra(carrito){
-    if (carrito.calcularTotal() !==0 ){
-        total.innerHTML = `
-            Usted debera abonar: <b>$${carrito.calcularTotal()}</b>. Sera redirigido para realizar el pago.`
-    }
-    else {
-        total.innerHTML = `
-            <b>Su carrito esta vacio.<b>`
-    }
+    carrito.calcularTotal() !==0 ?
+        alertComprar(carrito)
+        : 
+        total.innerHTML = `<b>Su carrito esta vacio.<b>`
 }
+
+function LimpiarCompra(carrito){
+    carrito.calcularTotal() !==0 ?
+        eliminarCarrito(carrito)
+        : 
+        total.innerHTML = `<b>Su carrito esta vacio.<b>`
+}
+
+function alertComprar(carrito) {
+    Swal.fire({
+        title: `Su total es de <b>$${carrito.calcularTotal()}</b>`,
+        icon: 'success',
+        footer: 'Usted sera redireccionado a MercadoPago',
+        showCancelButton: true,
+        cancelButtonText: `CANCELAR`,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'COMPRAR'
+    })
+    .then((result) => {
+        if (result.isConfirmed) {
+            let timerInterval
+            Swal.fire({
+                title: 'Redireccionando',
+                timer: 1000,
+                timerProgressBar: true,
+                didOpen: () => {
+                    Swal.showLoading()
+                    const b = Swal.getHtmlContainer().querySelector('b')
+                    timerInterval = setInterval(() => {
+                        b.textContent = Swal.getTimerLeft()
+                    }, 100)
+                },
+                willClose: () => {
+                    window.location.href = "https://www.mercadopago.com/"
+                    clearInterval(timerInterval)
+                }
+            })
+            .then((result) => {
+                if (result.dismiss === Swal.DismissReason.timer) {
+                    console.log('I was closed by the timer')
+                }
+            })
+        }
+    })
+}
+
+function alertLimpiar(){
+    Swal.fire(
+        'Se ha vaciado el carrito',
+        '',
+        'warning'
+    )
+}
+
+function alertRegistro(){
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 1500,
+        timerProgressBar: false,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+    Toast.fire({
+        icon: 'success',
+        title: 'Producto Registrado'
+    })
+}
+
+function alertAgregar(){
+    Toastify({
+        text: "Producto Añadido",
+        duration: 1500,
+        close: false,
+        gravity: "top",
+        position: "left", 
+        stopOnFocus: false,
+        style: {
+            background: "#0d6efd",
+        }
+    })
+    .showToast()
+}
+
+function main() {
+    inicializarElementos()
+    inicializarEventos()
+}
+
+main()
+
